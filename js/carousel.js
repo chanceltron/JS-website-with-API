47989c0d78bcac1b4766389b972ff86d09ddd505
Chance Ludwick <chanceludwick@gmail.com>
2022-10-07 16:04:19 -0500

    Leaving work/ going on vacation

---
+++ js/carousel.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+  const cards = document.querySelectorAll('.card');
+  const buttons = document.querySelectorAll('.slide-ctrl-container button');
+
+  let current = Math.floor(Math.random() * cards.length);
+  let next = current < cards.length - 1 ? current + 1 : 0;
+  let prev = current > 0 ? current - 1 : cards.length - 1;
+
+  const update = () => {
+    cards.forEach((card) => {
+      card.classList.remove('active', 'prev', 'next');
+    });
+  };
+
+  const goToNum = (newCurrentIndex) => {
+    current = newCurrentIndex;
+    next = current < slides.length - 1 ? current + 1 : 0;
+    prev = current > 0 ? current - 1 : slides.length - 1;
+    update();
+  };
+
+  cards[current].classList.add('active');
+  cards[prev].classList.add('prev');
+  cards[next].classList.add('next');
+
+  const goToNext = () => (current < cards.length - 1 ? goToNum(current + 1) : goToNum(0));
+
+  const goToPrev = () => (current > 0 ? goToNum(current - 1) : goToNum(cards.length - 1));
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', () => (i === 0 ? goToPrev() : goToNext()));
+  }
+}, 500);
--- js/main.js
+++ js/main.js
@@ -1,16 +1,67 @@
-const pokeapi = fetch('https://pokeapi.co/api/v2/pokemon/1');
-const allPokemon = [];
+const pokeapi = 'https://pokeapi.co/api/v2/pokemon/';
 
-for (let i = 1; i < 152; i++) {
-  let currentPokemon = fetch(`https://pokeapi.co/api/v2/pokemon/${[i]}`);
-  // currentPokemon.then((data) => data.json()).then((data) => console.log(data));
-  currentPokemon
-    .then((data) => data.json())
-    .then((data) => allPokemon.push({ id: data.id, name: data.name, type: data.types[0].type.name, abilities: data.abilities, image: data.sprites.front_default }));
+const root = document.documentElement;
+
+const cardWrapper = document.querySelector('.card-wrapper');
+
+const buildCard = (pokemon) => {
+  let cardDiv = document.createElement('div');
+  cardDiv.classList.add('card', `${pokemon.type}`);
+  cardDiv.style.backgroundImage = `url(../assets/svg/${pokemon.type}-card-bg.svg)`;
+  cardDiv.innerHTML = `
+  <div class="card-header">
+    <div class="pokemon-name">${pokemon.name}</div>
+    <div class="pokemon-info">
+      <div class="pokemon-hp">${pokemon.hp} HP</div>
+      <div class="pokemon-type">
+        <img src="assets/png/${pokemon.type}-type.png" alt="" />
+      </div>
+    </div>
+  </div>
+  <div class="pokemon-sprite-wrapper">
+    <img src="https://img.pokemondb.net/artwork/${pokemon.name}.jpg" alt="" />
+  </div>
+  `;
+  let abilitiesContainer = document.createElement('div');
+  abilitiesContainer.classList.add('pokemon-abilities');
+  pokemon.abilities.forEach((ability) => {
+    const abilityName = ability.ability.name;
+    let abilityDiv = document.createElement('div');
+    abilityDiv.innerText = `${abilityName}`;
+    abilitiesContainer.appendChild(abilityDiv);
+  });
+
+  cardWrapper.appendChild(cardDiv);
+  cardDiv.appendChild(abilitiesContainer);
+};
+
+// pokemonImage = (res,)
+
+const getPokemon = async (url) => {
+  const rawData = await (await fetch(url)).json();
+  const pokemon = await {
+    id: rawData.id,
+    name: rawData.name,
+    hp: rawData.stats[0].base_stat,
+    type: rawData.types[0].type.name,
+    abilities: rawData.abilities,
+    image: 'https://img.pokemondb.net/artwork/' + rawData.name + '.jpg',
+  };
+  await buildCard(pokemon);
+};
+
+// getPokemon(pokeapi + 4);
+
+for (let i = 0; i < 30; i++) {
+  const excludedList = [0, 774, 778];
+  const randomNumber = () => {
+    let number = Math.floor(Math.random() * 905);
+    if (excludedList.includes(number)) {
+      randomNumber();
+    } else {
+      return number;
+    }
+  };
+
+  getPokemon(pokeapi + randomNumber());
 }
-
-pokeapi.then((data) => data.json()).then((data) => console.log(data));
-
-setTimeout(() => {
-  console.log(allPokemon);
-}, 1000);
