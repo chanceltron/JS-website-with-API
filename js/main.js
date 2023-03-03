e5fa840a9c133f1cb612d61868e4318f177e33bd
Chance Ludwick <chanceludwick@gmail.com>
2023-03-03 13:59:16 -0600

    Complete add and remove team members functionality fix sorting functionality when removing team members Refactor code

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 const pokeapi = 'https://pokeapi.co/api/v2/pokemon/';
 const root = document.documentElement;
 const cardWrapper = document.querySelector('.card-wrapper');
-const favoritesContainer = document.getElementById('favoritesContainer');
+const teamContainer = document.getElementById('favoritesContainer');
 const typeList = document.getElementById('typeList');
+const sortAZ = document.getElementById('sortAZ');
+const sortZA = document.getElementById('sortZA');
+const sort19 = document.getElementById('sort19');
+const sort91 = document.getElementById('sort91');
 
 /* Modal */
 const modalOpen = '[data-open]';
@@ -25,13 +29,36 @@ closeModal.forEach((elm) => {
   });
 });
 
-//
+// Data Structures
+const allPokemon = [];
 const favorites = [];
 const types = {};
 
-// Helpers
-const getRandomNum = (maxNum) => {
-  return Math.floor(Math.random() * maxNum) + 1;
+// Helper Functions
+let sortType = 'id';
+let sortReverse = false;
+const getRandomNum = (maxNum) => Math.floor(Math.random() * maxNum) + 1;
+const sort = (type, reverse) => {
+  if (type === 'id') {
+    !reverse
+      ? allPokemon.sort((a, b) => a.id - b.id)
+      : allPokemon.sort((a, b) => b.id - a.id);
+  } else if (sortType === 'name') {
+    allPokemon.sort((a, b) =>
+      !reverse ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
+    );
+  }
+  while (cardWrapper.firstChild) {
+    cardWrapper.removeChild(cardWrapper.firstChild);
+  }
+  buildAllPokemonCards();
+};
+const countTypes = (currType) => {
+  if (Object.keys(types).includes(currType)) {
+    let count = types[currType];
+    return (types[currType] = count + 1);
+  }
+  return (types[currType] = 1);
 };
 
 //
@@ -46,72 +73,47 @@ const getPokemon = async (url) => {
       type: data.types[0].type.name,
       moves: [
         // Randomly generate two moves for each Pokemon
-        { name: data.moves[getRandomNum(data.moves.length)].move.name },
-        { name: data.moves[getRandomNum(data.moves.length)].move.name },
+        await getPokemonMove(
+          data.moves[getRandomNum(data.moves.length)].move.name
+        ),
+        await getPokemonMove(
+          data.moves[getRandomNum(data.moves.length)].move.name
+        ),
       ],
       image: 'https://img.pokemondb.net/artwork/' + data.name + '.jpg',
     };
-    const updatedPokemon = await updatePokemonMoves(pokemon);
-    await addPokemonTypes(updatedPokemon);
-    const card = await buildCard(updatedPokemon);
-    const cardWithFav = await addFavBtn(card, updatedPokemon);
-    await cardWrapper.appendChild(cardWithFav);
-  } catch (err) {
-    return;
-  }
-};
-
-const updatePokemonMoves = async (pokemon) => {
-  const move1 = await getPokemonMove(pokemon.moves[0].name);
-  const move2 = await getPokemonMove(pokemon.moves[1].name);
-  return {
-    ...pokemon,
-    moves: [move1, move2],
-  };
+    await allPokemon.push(pokemon);
+    await countTypes(pokemon.type);
+  } catch (err) {}
 };
 
 const getPokemonMove = (moveName) => {
   return fetch(`https://pokeapi.co/api/v2/move/${moveName}`)
     .then((response) => response.json())
-    .then((data) => {
+    .then((move) => {
       // This gets the Effect Chance for each move that has a random chance
-      if (data.effect_entries[0].short_effect.includes('$effect_chance')) {
-        data.effect_entries[0].short_effect =
-          data.effect_entries[0].short_effect.replace(
-            '$effect_chance',
-            data.effect_chance
-          );
+      const chance = move.effect_chance;
+      let effect = move.effect_entries[0].short_effect;
+      if (effect.includes('$effect_chance')) {
+        effect = effect.replace('$effect_chance', chance);
       }
       return {
-        name: data.name,
-        description: data.effect_entries[0].short_effect,
+        name: move.name,
+        description: effect,
       };
-    })
-    .catch();
+    });
 };
 
-addPokemonTypes = (pokemon) => {
-  const currType = pokemon.type;
-  if (Object.keys(types).includes(currType)) {
-    let count = types[currType];
-    types[currType] = count + 1;
-    return;
-  }
-  types[currType] = 1;
-};
-
-const addPokemonToTeam = () => {
-  while (favoritesContainer.firstChild) {
-    favoritesContainer.removeChild(favoritesContainer.firstChild);
-  }
-  favorites.map((fav) => {
-    const favoriteCard = buildCard(fav);
-    const favoriteCardWithRemove = addRemoveBtn(favoriteCard, fav);
-    favoritesContainer.appendChild(favoriteCardWithRemove);
+const buildAllPokemonCards = () => {
+  allPokemon.map((pokemon) => {
+    const card = buildIndividualCard(pokemon);
+    const cardWithAddBtn = addFavBtn(card, pokemon);
+    cardWrapper.appendChild(cardWithAddBtn);
   });
 };
 
-const buildCard = (pokemon) => {
+const buildIndividualCard = (pokemon) => {
+  // Build Main Portion of Card
   let cardDiv = document.createElement('div');
   cardDiv.classList.add('card', `${pokemon.type}`);
   cardDiv.style.backgroundImage = `url(../assets/svg/${pokemon.type}-card-bg.svg)`;
@@ -128,21 +130,46 @@ const buildCard = (pokemon) => {
   <div class="pokemon-sprite-wrapper">
     <img src=${pokemon.image} />
   </div>
+  <div class="pokemon-moves">
+    <div>
+      <span class="move-name">${pokemon.moves[0].name}</span>
+      <span class="move-description">${pokemon.moves[0].description}</span>
+    </div>
+    <div>
+      <span class="move-name">${pokemon.moves[1].name}</span>
+      <span class="move-description">${pokemon.moves[1].description}</span>
+    </div>
+  </div>
   `;
-  let movesContainer = document.createElement('div');
-  movesContainer.classList.add('pokemon-moves');
-  pokemon.moves.map((move) => {
-    let moveDiv = document.createElement('div');
-    moveDiv.innerHTML = `
-    <span class='move-name'>${move.name}</span>
-    <span class='move-description'>${move.description}</span>
-    `;
-    movesContainer.appendChild(moveDiv);
-    cardDiv.appendChild(movesContainer);
-  });
   return cardDiv;
 };
 
+const teamManagementHandler = async (target, pokemon) => {
+  const card = target.parentElement.parentElement;
+  const addBtn = card.querySelector('.favorite-btn');
+  const removeBtn = card.querySelector('.remove-btn');
+
+  if (target.classList.contains('favorite-btn')) {
+    if (favorites.length < 6) {
+      allPokemon.splice(allPokemon.indexOf(pokemon), 1);
+      await favorites.push(pokemon);
+      addBtn.parentElement.remove();
+      const cardWithRemove = addRemoveBtn(card, pokemon);
+      teamContainer.appendChild(cardWithRemove);
+    } else {
+      alert('You can only have 6 pokemon on your team!');
+    }
+  } else if (target.classList.contains('remove-btn')) {
+    favorites.splice(favorites.indexOf(pokemon), 1);
+    await allPokemon.push(pokemon);
+    removeBtn.parentElement.remove();
+    const cardWithAdd = addFavBtn(card, pokemon);
+    cardWrapper.appendChild(cardWithAdd);
+  }
+  await sort(sortType, sortReverse);
+};
+
+// Add Favorite Button Overlay to Card
 const addFavBtn = (cardDiv, pokemon) => {
   let cardPopupBox = document.createElement('div');
   cardPopupBox.classList.add('card-popup-box');
@@ -151,12 +178,13 @@ const addFavBtn = (cardDiv, pokemon) => {
   `;
   cardDiv.appendChild(cardPopupBox);
   const favoriteBtn = cardDiv.querySelector('.favorite-btn');
-  favoriteBtn.addEventListener('click', () => {
-    teamAddHandler(pokemon);
+  favoriteBtn.addEventListener('click', (e) => {
+    teamManagementHandler(e.target, pokemon);
   });
   return cardDiv;
 };
 
+// Add Remove Button Overlay to Card
 const addRemoveBtn = (cardDiv, pokemon) => {
   let cardPopupBox = document.createElement('div');
   cardPopupBox.classList.add('card-popup-box');
@@ -165,39 +193,52 @@ const addRemoveBtn = (cardDiv, pokemon) => {
   `;
   cardDiv.appendChild(cardPopupBox);
   const removeBtn = cardDiv.querySelector('.remove-btn');
-  removeBtn.addEventListener('click', () => {
-    removeFromTeamHandler(pokemon);
+  removeBtn.addEventListener('click', (e) => {
+    teamManagementHandler(e.target, pokemon);
   });
   return cardDiv;
 };
 
-const teamAddHandler = (pokemon) => {
-  if (favorites.length < 6) {
-    favorites.push(pokemon);
-    addPokemonToTeam();
-  } else {
-    alert('You can only have 6 pokemon on your team!');
-  }
-};
-
-const removeFromTeamHandler = (pokemon) => {
-  const index = favorites.indexOf(pokemon);
-  favorites.splice(index, 1);
-  addPokemonToTeam();
-};
-
+// Build the Type List
 const buildTypeCount = () => {
-  const arrayOfTypes = Object.entries(types);
-  arrayOfTypes.forEach((type) => {
-    typeList.innerHTML += `<li>${type[0]} type: ${type[1]}</li>`;
+  Object.entries(types).forEach(([type, count]) => {
+    typeList.innerHTML += `<li>${type} type: ${count}</li>`;
   });
 };
 
-const start = async () => {
+// Sort Event Listeners
+sortAZ.addEventListener('click', () => {
+  sortType = 'name';
+  sortReverse = false;
+  sort(sortType, sortReverse);
+});
+sortZA.addEventListener('click', () => {
+  sortType = 'name';
+  sortReverse = true;
+  sort(sortType, sortReverse);
+});
+sort19.addEventListener('click', () => {
+  sortType = 'id';
+  sortReverse = false;
+  sort(sortType, sortReverse);
+});
+sort91.addEventListener('click', () => {
+  sortType = 'id';
+  sortReverse = true;
+  sort(sortType, sortReverse);
+});
+
+// Start the app
+const start = () => {
+  const urls = [];
   for (let i = 0; i < 30; i++) {
-    await getPokemon(pokeapi + getRandomNum(151));
+    urls.push(pokeapi + getRandomNum(151));
   }
-  await buildTypeCount();
+  Promise.all(urls.map((url) => getPokemon(url))).then(() => {
+    sort(sortType, sortReverse);
+    buildAllPokemonCards();
+    buildTypeCount();
+  });
 };
 
 start();
